cd3dcbcb18b92b2be905e8eb757a194da2794b4d
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-03-26 19:58:29 +0300

    chore view

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -14,8 +14,10 @@ module ProductsHelper
     }
     result[:available] = true if product.rt || product.dr
 
-    result[:html] += "<p>RT: ID: <a href=/rts?q[id_eq]=#{product.rt.id}>#{product.rt.id}</a>, остаток: #{product.rt.quantity}, цена: #{product.rt.price}</p>" if product.rt.present?
-    result[:html] += "<p>DR: ID: <a href=/drs?q[id_eq]=#{product.dr.id}>#{product.dr.id}</a>, остаток: #{product.dr.quantity}, цена: #{product.dr.price}</p>" if product.dr.present?
+    result[:html] += "<p>RT: ID: <a href=/rts?q%5Bid_eq%5D=#{product.rt.id}>#{product.rt.id}</a>, остаток: #{product.rt.quantity}, цена: #{product.rt.price}</p>" if product.rt.present?
+    result[:html] += "<p>DR: ID: <a href=/drs?q%5Bid_eq%5D=#{product.dr.id}>#{product.dr.id}</a>, остаток: #{product.dr.quantity}, цена: #{product.dr.price}</p>" if product.dr.present?
+    # result[:html] += "<p>RT: ID: <a href=/rts?q[id_eq]=#{product.rt.id}>#{product.rt.id}</a>, остаток: #{product.rt.quantity}, цена: #{product.rt.price}</p>" if product.rt.present?
+    # result[:html] += "<p>DR: ID: <a href=/drs?q[id_eq]=#{product.dr.id}>#{product.dr.id}</a>, остаток: #{product.dr.quantity}, цена: #{product.dr.price}</p>" if product.dr.present?
     result
   end
 end
